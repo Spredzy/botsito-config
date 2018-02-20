31b9937741e8d3e7dafa2f88c6841c41058fe598
Yanis Guenane <yanis@guenane.org>
2018-02-20 15:23:33 +0100

    Initial commit

---
+++ config.pl
@@ -0,0 +1,29 @@
+%% -*- prolog -*-
+
+:- module(config,
+          [config/2
+          ]).
+
+%% settings for irc.pl module
+
+config(irc_server, 'irc.freenode.net').
+config(irc_port, 6667).
+config(irc_nick, "stagiaire").
+
+% the first chan will get the notifications
+config(irc_channels, ["###testbot"]).
+
+%% add the optional modules you want to activate to the list
+config(modules, [dlrn, irc, self, autoupdate, github, puddle, fun]).
+
+%% dlrn and github settings
+config(dlrn_status_url, ["systemd", "master", "http://38.145.33.116/systemd-master/"]).
+config(dlrn_status_url, ["ansible", "devel", "http://38.145.33.116/ansible-devel/"]).
+
+config(github, ["systemd", "systemd"]).
+config(github, ["ansible", "ansible"]).
+
+config(gitrepo, ["ansible-distgit", "git://pkgs.fedoraproject.org/rpms/ansible"]).
+config(gitrepo, ["systemd-distgit", "git://pkgs.fedoraproject.org/rpms/systemd"]).
+
+%% config.pl ends here
